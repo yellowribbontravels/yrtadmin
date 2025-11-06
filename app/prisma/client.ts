934cc2419a3ea5e5d7295f1ae480c58b31d73f1a
Al Aqmar <alaqmarak0810@gmail.com>
2025-11-06 14:34:11 +0530

    deploy

--- app/prisma/client.ts
+++ app/prisma/client.ts
@@ -43,6 +43,10 @@ export { Prisma }
 path.join(__dirname, "query_engine-windows.dll.node")
 path.join(process.cwd(), "app/prisma/query_engine-windows.dll.node")
 
+// file annotations for bundling tools to include these files
+path.join(__dirname, "libquery_engine-rhel-openssl-3.0.x.so.node")
+path.join(process.cwd(), "app/prisma/libquery_engine-rhel-openssl-3.0.x.so.node")
+
 /**
  * Model Package
  * 
